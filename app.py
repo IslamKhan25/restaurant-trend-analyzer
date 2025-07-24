c6ac36338092d948e2d89a464da2b4f01fa4ad05
IslamKhan25 <khanislam7178@gmail.com>
2025-07-24 18:57:01 +0530

    app.py

--- app.py
+++ app.py
@@ -1,1 +1,4 @@
-# Streamlit App Placeholder
+uploaded_file = st.file_uploader("Upload Zomato CSV", type=["csv"])
+if uploaded_file is not None:
+    df = pd.read_csv(uploaded_file)
+    st.write(df.head())
